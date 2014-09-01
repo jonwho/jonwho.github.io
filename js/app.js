91ddc96f4d88dcbedc08808eb18a6b663648d456
jonwho <housuilun@yahoo.com>
2014-08-31 20:25:44 -0700

    formatting

--- js/app.js
+++ js/app.js
@@ -8,22 +8,23 @@ angular
 		'jonwho.controllers'
 	]).
 	config(['$routeProvider', function($routeProvider) {
-		$routeProvider.when('/', {
-			templateUrl: 'views/home.html'
-		})
-		.when('/about', {
-			templateUrl: 'views/about.html'
-		})
-		.when('/contact', {
-			templateUrl: 'views/contact.html'
-		})
-		.when('/calendar', {
-			templateUrl: 'views/calendar.html'
-		})
-		.when('/resume', {
-			templateUrl: 'views/resume.html'
-		});
-		.otherwise({
-			redirectTo: '/'
-		});
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/home.html'
+			})
+			.when('/about', {
+				templateUrl: 'views/about.html'
+			})
+			.when('/contact', {
+				templateUrl: 'views/contact.html'
+			})
+			.when('/calendar', {
+				templateUrl: 'views/calendar.html'
+			})
+			.when('/resume', {
+				templateUrl: 'views/resume.html'
+			});
+			.otherwise({
+				redirectTo: '/'
+			});
 	}]);

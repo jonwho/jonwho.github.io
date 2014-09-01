5e2cd3f285a5efefa7dddd5da56d69d5d55f448a
jonwho <housuilun@yahoo.com>
2014-08-31 19:42:59 -0700

    fix link

--- js/app.js
+++ js/app.js
@@ -7,10 +7,10 @@ angular.module('jonwho', [
 	'jonwho.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/', {templateUrl: 'main.html'});
+	$routeProvider.when('/', {templateUrl: 'home.html'});
 	$routeProvider.when('/about', {templateUrl: 'views/about.html'});
 	$routeProvider.when('/contact', {templateUrl: 'views/contact.html'});
 	$routeProvider.when('/calendar', {templateUrl: 'views/calendar.html'});
 	$routeProvider.when('/resume', {templateUrl: 'views/resume.html'});
-	$routeProvider.otherwise({redirectTo: '/index.html'});
+	$routeProvider.otherwise({redirectTo: '/home.html'});
 }]);

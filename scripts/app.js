eeb41629fd7e68e58ad268c1cc0338e3984d6353
jonwho <housuilun@yahoo.com>
2014-09-02 21:12:01 -0700

    nav bar links work with ui-router

--- scripts/app.js
+++ scripts/app.js
@@ -9,27 +9,56 @@ angular
 		'ui.router',
 		'jonwho.controllers'
 	]).
-	config(['$routeProvider', function($routeProvider) {
-		$routeProvider
-			.when('/', {
+	// config(['$routeProvider', function($routeProvider) {
+	// 	$routeProvider
+	// 		.when('/', {
+	// 			templateUrl: 'views/home.html'
+	// 		})
+	// 		.when('/home', {
+	// 			templateUrl: 'views/home.html'
+	// 		})
+	// 		.when('/about', {
+	// 			templateUrl: 'views/about.html'
+	// 		})
+	// 		.when('/contact', {
+	// 			templateUrl: 'views/contact.html'
+	// 		})
+	// 		.when('/calendar', {
+	// 			templateUrl: 'views/calendar.html'
+	// 		})
+	// 		.when('/resume', {
+	// 			templateUrl: 'views/resume.html'
+	// 		})
+	// 		.otherwise({
+	// 			redirectTo: '/'
+	// 		});
+	// }]);
+
+	// use ui-router instead
+	config(function($stateProvider, $urlRouterProvider) {
+
+		$urlRouterProvider.otherwise('/home');
+
+		$stateProvider
+			// home states and nested views
+			.state('home', {
+				url: '/home',
 				templateUrl: 'views/home.html'
 			})
-			.when('/home', {
-				templateUrl: 'views/home.html'
-			})
-			.when('/about', {
+			.state('about', {
+				url: '/about',
 				templateUrl: 'views/about.html'
 			})
-			.when('/contact', {
+			.state('contact', {
+				url: '/contact',
 				templateUrl: 'views/contact.html'
 			})
-			.when('/calendar', {
+			.state('calendar', {
+				url: '/calendar',
 				templateUrl: 'views/calendar.html'
 			})
-			.when('/resume', {
+			.state('resume', {
+				url: '/resume',
 				templateUrl: 'views/resume.html'
-			})
-			.otherwise({
-				redirectTo: '/'
 			});
-	}]);
+	});

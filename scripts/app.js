c5fd7bb82b370b4a1eeef0d21e6dea88db73513c
jonwho <housuilun@yahoo.com>
2014-09-02 22:40:45 -0700

    remove ngRoute components

--- scripts/app.js
+++ scripts/app.js
@@ -4,37 +4,10 @@
 // Declare app level module which depends on filters, and services
 angular
 	.module('jonwho', [
-		'ngRoute',
 		'ngResource',
 		'ui.router',
 		'jonwho.controllers'
 	]).
-	// config(['$routeProvider', function($routeProvider) {
-	// 	$routeProvider
-	// 		.when('/', {
-	// 			templateUrl: 'views/home.html'
-	// 		})
-	// 		.when('/home', {
-	// 			templateUrl: 'views/home.html'
-	// 		})
-	// 		.when('/about', {
-	// 			templateUrl: 'views/about.html'
-	// 		})
-	// 		.when('/contact', {
-	// 			templateUrl: 'views/contact.html'
-	// 		})
-	// 		.when('/calendar', {
-	// 			templateUrl: 'views/calendar.html'
-	// 		})
-	// 		.when('/resume', {
-	// 			templateUrl: 'views/resume.html'
-	// 		})
-	// 		.otherwise({
-	// 			redirectTo: '/'
-	// 		});
-	// }]);
-
-	// use ui-router instead
 	config(function($stateProvider, $urlRouterProvider) {
 
 		$urlRouterProvider.otherwise('/home');

cf63b79c203fd75a530b6f418f31fd3a407a86d8
Jon Ho <housuilun@yahoo.com>
2015-03-02 02:59:01 -0800

    remove calendar tab and add projects tab

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -6,6 +6,9 @@ angular.module('jonwho.controllers', [])
   .controller('HomeCtrl', ['$scope', function($scope) {
   	$scope.msg = 'home';
   }])
+  .controller('CarouselCtrl', ['$scope', function($scope) {
+  	
+  }])
   .controller('MyCtrl2', ['$scope', function($scope) {
   	$scope.msg = 'nother ctrl';
   }]);

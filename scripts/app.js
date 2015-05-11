d50ec0b4b378c45dcd109534aae864a7f00fe354
Jon Ho <housuilun@yahoo.com>
2015-05-11 04:51:00 -0700

    more ui mocks

--- scripts/app.js
+++ scripts/app.js
@@ -20,14 +20,6 @@ app.config(function($stateProvider, $urlRouterProvider) {
       });
 });
 
-app.config(function($mdIconProvider) {
-  $mdIconProvider
-    .icon('github', '../bower_components/ionicons/src/social-octocat.svg', 24)
-    .icon('linkedin', '../bower_components/ionicons/src/social-linkedin.svg', 24)
-    .icon('twitter', '../bower_components/ionicons/src/social-twitter.svg', 24)
-    .icon('email', '../bower_components/ionicons/src/email.svg', 24);
-});
-
 // TODO: Try and match to UCSD blue/yellow colors
 // Theming Material Design
 // app.config(function($mdThemingProvider) {

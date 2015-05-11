ad3aa1cffe7f04b07597a5a19cf192d8f9329957
Jon Ho <housuilun@yahoo.com>
2015-05-11 00:55:27 -0700

    got floating button working as footer

--- scripts/app.js
+++ scripts/app.js
@@ -19,3 +19,19 @@ app.config(function($stateProvider, $urlRouterProvider) {
         templateUrl: '../index.html'
       });
 });
+
+app.config(function($mdIconProvider) {
+  $mdIconProvider
+    .icon('github', '../bower_components/ionicons/src/social-octocat.svg', 24)
+    .icon('linkedin', '../bower_components/ionicons/src/social-linkedin.svg', 24)
+    .icon('twitter', '../bower_components/ionicons/src/social-twitter.svg', 24)
+    .icon('email', '../bower_components/ionicons/src/email.svg', 24);
+});
+
+// TODO: Try and match to UCSD blue/yellow colors
+// Theming Material Design
+// app.config(function($mdThemingProvider) {
+//   $mdThemingProvider.theme('default')
+//     .primaryPalette('pink')
+//     .accentPalette('orange');
+// });

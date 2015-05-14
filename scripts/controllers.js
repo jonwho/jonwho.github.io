b1a3a0fd01fd6839bef095014096b2146f7fc549
Jon Ho <housuilun@yahoo.com>
2015-05-14 03:41:48 -0700

    better mock up

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,35 +1,39 @@
 'use strict';
 
 angular
-  .module('users')
-  .controller('NavCtrl', [
-             '$mdSidenav',
-             NavCtrl
-  ]);
+  .module('jonwho')
+  .controller('NavCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
+    $scope.users = [
+      {
+        name: 'Jon Ho',
+        avatar: '../bower_components/ionicons/src/person.svg',
+        templateUrl: '../views/test1.html'
+      },
+      {
+        name: 'Experience',
+        avatar: '../bower_components/ionicons/src/briefcase.svg',
+        templateUrl: '../views/test2.html'
+      },
+      {
+        name: 'Projects',
+        avatar: '../bower_components/ionicons/src/code.svg',
+        templateUrl: '../views/test1.html'
+      },
+      {
+        name: 'Resume',
+        avatar: '../bower_components/ionicons/src/document-text.svg',
+        templateUrl: '../views/test2.html'
+      }
+    ];
 
-function NavCtrl($mdSidenav) {
-  var self = this;
+    $scope.selected = $scope.users[0];
 
-  self.selected     = null;
-  self.users        = [ ];
-  self.selectUser   = selectUser;
-  self.toggleList   = toggleUsersList;
-  self.share        = share;
+    $scope.selectUser = function(user) {
+      $scope.selected = angular.isNumber(user) ? $scope.users[user] : user;
+      $scope.toggleList();
+    };
 
-  // *********************************
-  // Internal methods
-  // *********************************
-
-  /**
-    * Select the current avatars
-    * @param menuId
-    */
-  function selectUser ( user ) {
-    self.selected = angular.isNumber(user) ? $scope.users[user] : user;
-    self.toggleList();
-  }
-
-  function toggleUsersList() {
-    $mdSidenav('left').toggle();
-  }
-}
+    $scope.toggleList = function() {
+      $mdSidenav('left').toggle();
+    };
+  }]);
